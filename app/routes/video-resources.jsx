7a18adb1363755ad710b6391fcadf27276687e02
Isabelle <igracehu@gmail.com>
2026-04-03 14:08:10 -0700

    users with admin role can create video resources

--- app/routes/video-resources.jsx
+++ app/routes/video-resources.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
+import { supabase } from "../supabase";
 import { Menu } from "../components/menu";
 import { Footer } from "../components/footer";
 import { PopupForm } from "../components/popup";
@@ -27,6 +29,22 @@ const resources = [
     }
 ]
 
+async function createVideoResource(formData) {
+    const { error } = await supabase
+        .from('video resources')
+        .insert({ 
+            title: formData.get("vid-resource-title"),
+            video_url: formData.get("vid-resource-link"),
+            video_thumbnail: formData.get("vid-resource-thumbnail"),
+            video_description: formData.get("vid-resource-desc"),
+            speaker: formData.get("vid-resource-speaker-name"),
+            speaker_university: formData.get("vid-resource-speaker-uni"),
+            speaker_image: formData.get("vid-resource-speaker-img"),
+            speaker_details: formData.get("vid-resource-speaker-desc"),
+         })
+    return error;
+}
+
 function ResourceCard({resourceInfo}) {
     return (
         <div className="resource-card">
@@ -50,59 +68,8 @@ function ResourceCard({resourceInfo}) {
     )
 }
 
-function CreateVidRPopup({showPopup, setShowPopup, userId}) {
-    function validate(formData) {
-
-    }
-    return (
-        <PopupForm show={showPopup} setShow={setShowPopup} validate={validate}>
-            <h4>Create new video resource</h4>
-            <div className="grid md:grid-cols-2 grid-cols-1 gap-5 mb-5">
-                <div>
-                    <label for="vid-resource-title">Video resource title:</label><br />
-                    <input id="vid-resource-title" name="vid-resource-title" type="text" className="input input-text w-full" />
-                </div>
-                <label>
-                    Video resource thumbnail image:
-                    <input id="vid-resource-thumbnail" name="vid-resource-thumbnail" type="file" />
-                    <div className="input-error">This field is required.</div>
-                </label>
-            </div>
-            
-            <label for="vid-resource-link">Video resource link:</label><br />
-            <input id="vid-resource-link" name="vid-resource-link" type="text" className="input input-text w-full" />
-            <br /><br />
-            <label for="vid-resource-desc">Video description:</label><br />
-            <textarea id="vid-resource-desc" name="vid-resource-desc" className="input input-text w-full h-30"></textarea>
-            <br/> <br/>
-
-            <fieldset className="border-t-2 border-gray-light pt-4">
-                <div className="text-sm font-semibold text-secondary-dark">Speaker Details</div>
-                <div className="mt-3 grid md:grid-cols-2 grid-cols-1 gap-5">
-                    <div>
-                        <label for="vid-resource-speaker-name">Name:</label><br />
-                        <input id="vid-resource-speaker-name" name="vid-resource-speaker-name" type="text" className="input input-text w-full" />
-                    </div>
-                    <div>
-                        <label for="vid-resource-speaker-uni">University:</label><br />
-                        <input id="vid-resource-speaker-uni" name="vid-resource-speaker-uni" type="text" className="input input-text w-full" />
-                    </div>
-                </div>
-                <br/>
-                <label>
-                    Speaker image:
-                    <input id="vid-resource-speaker-img" name="vid-resource-speaker-img" type="file" className="ml-3" />
-                    <div className="input-error">This field is required.</div>
-                </label>
-                <br /><br />
-                <label for="vid-resource-speaker-desc">Description:</label><br />
-                <textarea id="vid-resource-speaker-desc" name="vid-resource-speaker-desc" className="input input-text w-full h-20"></textarea>
-            </fieldset>
-        </PopupForm>
-    )
-}
-
 export default function VideoResources() {
+    const navigate = useNavigate();
     const isAdmin = true;
     const [showPopup, setShowPopup] = useState(false);
     const [currentPage, setCurrentPage] = useState(0);
@@ -117,18 +84,111 @@ export default function VideoResources() {
         }
     }
 
+    const [formRequired, setFormRequired] = useState({
+        vidResourceTitle: "",
+        vidResourceLink: "",
+        vidResourceSpeakerName: "",
+    })
+    const [hasError, setHasError] = useState(false);
+
+    async function validate(formData) {
+        let isValidated = true;
+        const isRequired = {
+            vidResourceTitle: formData.get('vid-resource-title') === (null || ""),
+            vidResourceLink: formData.get('vid-resource-link') === (null || ""),
+            vidResourceSpeakerName: formData.get('vid-resource-speaker-name') === (null || "")
+        }
+        for (let value of Object.values(isRequired)) {
+            if (value) {
+                isValidated = false;
+                break;
+            }
+        }
+        if (!isValidated) {
+            setFormRequired(isRequired);
+            return false;
+        }
+
+        const create = await createVideoResource(formData);
+        if (create === null) {
+            setShowPopup(false);
+            navigate(0);
+        } else {
+            setHasError(true);
+        }
+    }
+
+    function handleShowPopupForm() {
+        // This resets formRequired so that there are no error messages when the form is reloaded
+        setFormRequired({
+            vidResourceTitle: "",
+            vidResourceLink: "",
+            vidResourceSpeakerName: ""
+        })
+        setShowPopup(true);
+    }
+
     return (
         <>
             { isAdmin &&
                 <div className="z-1000 absolute top-0 left-0">
-                    <CreateVidRPopup showPopup={showPopup} setShowPopup={setShowPopup} userId={userId} />
+                    <PopupForm show={showPopup} setShow={setShowPopup} validate={validate} hasError={hasError}>
+                        <h4>Create new video resource</h4>
+                        <div className="grid md:grid-cols-2 grid-cols-1 gap-5 mb-5 relative">
+                            <div>
+                                <label for="vid-resource-title">Video resource title:</label><br />
+                                <input id="vid-resource-title" name="vid-resource-title" type="text" className={"input input-text w-full " + (formRequired?.vidResourceTitle && "input-required")} placeholder="Video title" />
+                                <div className="input-error">This field is required.</div>
+                            </div>
+                            <label>
+                                Video resource thumbnail image:
+                                <input id="vid-resource-thumbnail" name="vid-resource-thumbnail" type="file" />
+                                <div className="input-error">This field is required.</div>
+                            </label>
+                        </div>
+                        
+                        <div className="relative">
+                            <label for="vid-resource-link">Video resource link:</label><br />
+                            <p className="text-sm text-disabled-dark mt-1">This is the video link that will be embedded. Please only include the link and not the entire embed.</p>
+                            <input id="vid-resource-link" name="vid-resource-link" type="text" className={"input input-text w-full " + (formRequired?.vidResourceLink && "input-required")} placeholder="Video link"  />
+                            <div className="input-error">This field is required.</div>
+                            <br /><br />
+                            <label for="vid-resource-desc">Video description:</label><br />
+                            <textarea id="vid-resource-desc" name="vid-resource-desc" className="input input-text w-full h-30" placeholder="Enter your video description..."></textarea>
+                            <br/> <br/>
+                        </div>
+
+                        <fieldset className="border-t-2 border-gray-light pt-4 relative">
+                            <div className="text-sm font-semibold text-secondary-dark">Speaker Details</div>
+                            <div className="mt-3 grid md:grid-cols-2 grid-cols-1 gap-5">
+                                <div>
+                                    <label for="vid-resource-speaker-name">Name:</label><br />
+                                    <input id="vid-resource-speaker-name" name="vid-resource-speaker-name" type="text" className={"input input-text w-full " + (formRequired?.vidResourceSpeakerName && "input-required")} placeholder="Name" />
+                                    <div className="input-error">This field is required.</div>
+                                </div>
+                                <div>
+                                    <label for="vid-resource-speaker-uni">University:</label><br />
+                                    <input id="vid-resource-speaker-uni" name="vid-resource-speaker-uni" type="text" className="input input-text w-full" placeholder="University" />
+                                </div>
+                            </div>
+                            <br/>
+                            <label>
+                                Speaker image:
+                                <input id="vid-resource-speaker-img" name="vid-resource-speaker-img" type="file" className="ml-3" />
+                                <div className="input-error">This field is required.</div>
+                            </label>
+                            <br /><br />
+                            <label for="vid-resource-speaker-desc">Description:</label><br />
+                            <textarea id="vid-resource-speaker-desc" name="vid-resource-speaker-desc" className="input input-text w-full h-20" placeholder="Enter your speaker descrption..."></textarea>
+                        </fieldset>
+                    </PopupForm>
                 </div>
             }
             <Menu />
             <div className="py-20 px-10 lg:px-40 duration-200">
                 <div className="flex justify-between md:items-center md:flex-row flex-col md:mb-0 mb-5">
                     <h1>Video Resources</h1>
-                    { isAdmin && <button className="button" onClick={() => setShowPopup(true)}><i className="bi bi-plus-lg mr-3"></i>Create new video resource</button> }
+                    { isAdmin && <button className="button" onClick={handleShowPopupForm}><i className="bi bi-plus-lg mr-3"></i>Create new video resource</button> }
                 </div>
                 <p>
                     Here you will find some videos provided by members of our network that you can use for your own training and also share with your students.
