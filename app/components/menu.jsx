6b3b8e8763227d53b8133b8d9766c693044d66d7
Isabelle <igracehu@gmail.com>
2026-04-12 15:37:51 -0700

    menu updates

--- app/components/menu.jsx
+++ app/components/menu.jsx
@@ -35,43 +35,19 @@ export function Menu() {
 
   useEffect(() => {
 
-    if (meetingsDdActive) {
-      const meetingsDdEl = document.getElementById("meetings-dd");
-      meetingsDdEl.classList.remove("invisible");
-      meetingsDdEl.classList.remove("opacity-0");
-      meetingsDdEl.classList.add("mt-0");
-    } else {
-      const meetingsDdEl = document.getElementById("meetings-dd");
-      meetingsDdEl.classList.add("opacity-0");
-      meetingsDdEl.classList.add("invisible");
-      meetingsDdEl.classList.remove("mt-0");
+    const menuDropdownEls = document.getElementsByClassName("menu-dropdown-container");
+    console.log(menuDropdownEls);
+    for (let menuDropdown of menuDropdownEls) {
+      menuDropdown.addEventListener("mouseout", function() { menuDropdown.classList.remove("active"); });
+      menuDropdown.addEventListener("mouseover", function() { menuDropdown.classList.add("active"); });
+      console.log(menuDropdown);
     }
 
-    if (whatWeDoActive) {
-      const whatWeDoEl = document.getElementById("what-we-do-dd");
-      whatWeDoEl.classList.remove("invisible");
-      whatWeDoEl.classList.remove("opacity-0");
-      whatWeDoEl.classList.add("mt-0");
-    } else {
-      const whatWeDoEl = document.getElementById("what-we-do-dd");
-      whatWeDoEl.classList.add("opacity-0");
-      whatWeDoEl.classList.add("invisible");
-      whatWeDoEl.classList.remove("mt-0");
+    const sideMenuDropdownEls = document.getElementsByClassName("side-menu-dropdown-container");
+    for (let sideMenuDropdown of sideMenuDropdownEls) {
+      sideMenuDropdown.addEventListener("mouseover", function() { sideMenuDropdown.classList.add("active"); })
+      sideMenuDropdown.addEventListener("mouseout", function() { sideMenuDropdown.classList.remove("active"); })
     }
-    if (loggedIn) {
-      if (accountDdActive) {
-        const accountDdEl = document.getElementById("account-dd");
-        accountDdEl.classList.remove("invisible");
-        accountDdEl.classList.remove("opacity-0");
-        accountDdEl.classList.add("mt-1");
-      } else {
-        const accountDdEl = document.getElementById("account-dd");
-        accountDdEl.classList.add("opacity-0");
-        accountDdEl.classList.add("invisible");
-        accountDdEl.classList.remove("mt-1");
-      }
-    }
-    
 
     if (sideMenuActive) {
       const sideMenuContEl = document.getElementById("side-menu-container");
@@ -83,7 +59,7 @@ export function Menu() {
       sideMenuContEl.children[0].classList.add("hidden");
     }
 
-  }, [meetingsDdActive, whatWeDoActive, sideMenuActive, accountDdActive])
+  }, [sideMenuActive, accountDdActive, currentUser])
 
 
   return (
@@ -92,108 +68,151 @@ export function Menu() {
 
       </div>
 
-      <div className="relative flex justify-between content-center p-2 shadow-sm z-1">
-        <div className="flex content-center">
-          <button onClick={() => setSideMenuActive(!sideMenuActive)} className="relative md:hidden block px-4 text-primary-dark hover:cursor-pointer hover:text-secondary-light duration-200 bg-white z-1">
-            <i className="bi bi-list text-[1.7rem]"></i>
-          </button>
-          <NavLink to="/" end className="relative duration-200 hover:opacity-70 px-4 bg-white z-1">
-            <img className="w-18 h-full" src="/assets/logo.svg" />
-          </NavLink>
-        </div>
+      <div className="relative flex items-center content-center md:justify-start justify-between shadow-sm z-1 duration-200">
+        <button onClick={() => setSideMenuActive(!sideMenuActive)} className="relative md:hidden block px-4 text-primary-dark hover:cursor-pointer hover:text-secondary-light duration-200 bg-white z-1">
+          <i className="bi bi-list text-[1.7rem]"></i>
+        </button>
+        <NavLink to="/" end className="relative duration-200 hover:opacity-70 md:px-4 bg-white z-1 my-1 md:mr-auto">
+          <img className="h-[2.5rem]" src="/assets/logo.svg" />
+          {/* <img className="h-[1.5rem] ml-4 md:hidden block" src="/assets/logo-iajes-text.svg" /> */}
+        </NavLink>
 
-        <div className="flex content-center">
-          <div className="md:flex hidden">
-            <div onBlur={() => setWhatWeDoActive(false)} tabIndex="0">
-              <div className="link p-4" onClick={() => setWhatWeDoActive(!whatWeDoActive)}>
-                What We Do
-              </div>
-              <div id="what-we-do-dd" className="z-2 bg-white absolute flex flex-col rounded-md border border-gray-light shadow-sm duration-200 -mt-3 opacity-0 invisible">
-                <NavLink to="/about" end className="link p-4 pr-10">
-                  About IAJES
-                </NavLink>
-                <NavLink to="/organizational-structure" end className="link p-4 pr-10 pb-5">
-                  Organizational Structure
-                </NavLink>
-              </div>
+
+        <div className="relative flex items-center w-120 hidden md:flex">
+          <div className="menu-dropdown-container">
+            <div className="menu-dropdown-button link p-4">
+              What We Do
             </div>
-
-            <NavLink to="/task-forces" end className="link p-4">
-              Task Forces
-            </NavLink>
-
-            <NavLink to="/video-resources" end className="link p-4">
-              Video Resources
-            </NavLink>
-
-            <div onBlur={() => setMeetingsDdActive(false)} tabIndex="0">
-              <div className="link p-4" onClick={() => setMeetingsDdActive(!meetingsDdActive)}>
-                Meetings
-              </div>
-              <div id="meetings-dd" className="z-2 bg-white absolute flex flex-col rounded-md border border-gray-light shadow-sm duration-200 -mt-3 opacity-0 invisible">
-                <NavLink to="/regional-meetings" end className="link p-4 pr-10">
-                  Regional Meetings
-                </NavLink>
-                <NavLink to="/international-meetings" end className="link p-4 pr-10 pb-5">
-                  International Meetings
-                </NavLink>
-              </div>
-            </div>
-
-            <NavLink to="/newsletter" end className="link p-4">
-              Newsletter
-            </NavLink>
-
-            <NavLink to="/search" end className="link p-4">
-              People
-            </NavLink>
-          </div>
-
-          <div className="mx-2 flex items-center gap-3">
-            {!loggedIn &&
-              <NavLink to="/signin" end className="block button">
-                Sign In
+            <div className="menu-dropdown -mt-[240px] py-1">
+              <NavLink to="/about" className="link py-3 px-4 pr-10">
+                About IAJES
               </NavLink>
-            }
-            {loggedIn &&
-              <div onBlur={() => setAccountDdActive(false)} className="relative" tabIndex="0">
-                <div className="rounded-full bg-primary-dark w-12 h-12 p-4 hover:bg-secondary-light duration-200 cursor-pointer" onClick={() => setAccountDdActive(!accountDdActive)}>
-                </div>
-                <div id="account-dd" className="z-2 w-50 bg-white absolute right-0 flex flex-col rounded-md border border-gray-light shadow-sm duration-200 -mt-3 opacity-0 invisible">
-                  <NavLink to={"/profile/" + currentUser?.id} end className="p-4 pb-0">
-                    Profile
-                  </NavLink>
-                  <div end className="p-4">
-                    <button onClick={handleSignOut} className="block button">
-                      Sign Out
-                    </button>
-                  </div>
-                </div>
-              </div>
-            }
+              <NavLink to="/organizational-structure" className="link py-3 px-4 pr-10">
+                Organizational Structure
+              </NavLink>
+              <NavLink to="/newsletter" className="link py-3 px-4 pr-10">
+                Newsletter
+              </NavLink>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div id="side-menu-container" className="absolute w-full z-0 md:hidden">
-        <div id="side-menu-bg" className="absolute w-full h-svh top-0 left-0 z-0 bg-white opacity-10 hidden" onClick={() => setSideMenuActive(false)}></div>
-        <div id="side-menu" className="absolute bg-white flex flex-col w-70 h-dvh duration-400 ease-in-out shadow-md z-1 -left-70" >
-          <NavLink to="/about" end className="pt-10 px-7">
-            What We Do
-          </NavLink>
-          <NavLink to="/task-forces" end className="pt-10 px-7">
+          <NavLink to="/task-forces" className="link p-4">
             Task Forces
           </NavLink>
-          <NavLink to="/regional-meetings" end className="pt-10 px-7">
-            Regional Meetings
+
+          <div className="menu-dropdown-container">
+            <div className="menu-dropdown-button link p-4">
+              Resources
+            </div>
+            <div className="menu-dropdown -mt-[220px] py-1">
+              <NavLink to="/video-resources" end className="link py-3 px-4 pr-10">
+                Video Resources
+              </NavLink>
+              <NavLink to="/webinars" end className="link py-3 px-4 pr-10">
+                Webinars
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="menu-dropdown-container">
+            <div className="menu-dropdown-button link p-4">
+              Meetings
+            </div>
+            <div className="menu-dropdown -mt-[220px] py-1">
+              <NavLink to="/regional-meetings" end className="link py-3 px-4 pr-10">
+                Regional Meetings
+              </NavLink>
+              <NavLink to="/international-meetings" end className="link py-3 px-4 pr-10">
+                International Meetings
+              </NavLink>
+            </div>
+          </div>
+
+          <NavLink to="/search" end className="link p-4">
+            People
           </NavLink>
-          <NavLink to="/biennal-meetings" end className="pt-10 px-7">
-            Biennal Meetings
+
+        </div>
+
+        {!loggedIn &&
+          <NavLink to="/signin" end className="block button my-1.5">
+            Sign In
           </NavLink>
-          <NavLink to="/newsletter" end className="pt-10 px-7">
-            Newsletter
+        }
+        {loggedIn &&
+          <div className="menu-dropdown-container">
+            <div className="menu-dropdown-button rounded-full bg-primary-dark w-10 h-10 my-1.5 ml-2 mr-4 hover:bg-secondary-light duration-200 cursor-pointer" 
+                >
+            </div>
+            <div className="menu-dropdown right-3 -mt-[220px]">
+              <NavLink to={"/profile/" + currentUser?.id} end className="link p-4 pb-0">
+                Profile
+              </NavLink>
+              <div className="p-4">
+                <button onClick={handleSignOut} className="block button">
+                  Sign Out
+                </button>
+              </div>
+            </div>
+          </div>
+        }
+
+      </div>
+
+      <div id="side-menu-container" className="absolute w-full z-0">
+        <div id="side-menu-bg" className="absolute w-full h-svh top-0 left-0 z-0 bg-white opacity-10 hidden" onClick={() => setSideMenuActive(false)}></div>
+        <div id="side-menu" className="absolute bg-white flex flex-col w-70 h-dvh duration-400 ease-in-out shadow-md z-1 -left-70" >
+          
+          <div className="side-menu-dropdown-container mt-7 pb-3 px-7">
+            <div className="side-menu-dropdown-button link py-3">
+              What We Do
+            </div>
+            <div className="side-menu-dropdown flex flex-col border-l-2 border-primary-light">
+              <NavLink to="/about" end className="link py-2 px-4">
+                  About IAJES
+                </NavLink>
+              <NavLink to="/organizational-structure" end className="link py-2 px-4">
+                Organizational Structure
+              </NavLink>
+              <NavLink to="/newsletter" end className="link py-2 px-4">
+                Newsletter
+              </NavLink>
+            </div>
+          </div>
+
+          <NavLink to="/task-forces" end className="link py-3 pb-6 px-7">
+            Task Forces
           </NavLink>
-          <NavLink to="/search" end className="py-10 px-7">
+
+          <div className="side-menu-dropdown-container py-3 px-7">
+            <div className="side-menu-dropdown-button link pb-3">
+              Resources
+            </div>
+            <div className="side-menu-dropdown flex flex-col border-l-2 border-primary-light">
+              <NavLink to="/video-resources" end className="link py-2 px-4">
+                Video Resources
+              </NavLink>
+              <NavLink to="/webinars" end className="link py-2 px-4">
+                Webinars
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="side-menu-dropdown-container py-3 px-7">
+            <div className="side-menu-dropdown-button link pb-3">
+              Meetings
+            </div>
+            <div className="side-menu-dropdown flex flex-col border-l-2 border-primary-light">
+              <NavLink to="/regional-meetings" end className="link py-2 px-4">
+                Regional Meetings
+              </NavLink>
+              <NavLink to="/international-meetings" end className="link py-2 px-4">
+                International Meetings
+              </NavLink>
+            </div>
+          </div>
+
+          <NavLink to="/search" end className="link py-3 px-7">
             People
           </NavLink>
         </div>
