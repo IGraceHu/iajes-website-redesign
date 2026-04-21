374493f7b621faaa1f569406908759c2cebdc9af
Isabelle <igracehu@gmail.com>
2026-04-20 22:21:50 -0700

    updated task forces hub page

--- app/routes/taskforces.jsx
+++ app/routes/taskforces.jsx
@@ -1,4 +1,5 @@
 import { Menu } from "../components/menu";
+import { supabase } from "../supabase";
 import { Footer } from "../components/footer";
 import { Banner } from "../components/graphics";
 
@@ -59,23 +60,41 @@ const tfInfoTemp = [
     }
 ]
 
+async function getTaskForces() {
+    const { data, error } = await supabase
+        .from('task forces')
+        .select("id, name, url, short_desc");
+
+    if (data) {
+        data.sort((a, b) => { return a.id - b.id })
+    }
+
+    return data || [];
+}
+
+export async function loader({ params }) {
+  return await getTaskForces();
+}
+
 function TaskForceSection({taskForceInfo}) {
-    let cardClass = "flex w-full px-10 py-10 gap-10 duration-200";
+    let cardClass = "flex w-full px-15 py-10 gap-10 duration-200";
     return (
         <div className={cardClass}>
             <div className="w-full">
                 <a href={"/task-forces/" + taskForceInfo.url}><h2 className="text-center">{taskForceInfo.name}</h2></a>
-                <p>{taskForceInfo.desc}</p>
+                <p>{taskForceInfo.short_desc}</p>
             </div>
-            { taskForceInfo?.imageUrl && 
-            <img className="w-100 h-full min-h-50 max-h-75" src={taskForceInfo.imageUrl} />}
+            { taskForceInfo?.image_url && 
+            <img className="w-100 h-full min-h-50 max-h-75" src={taskForceInfo.image_url} />}
         </div>
     )
 }
 
-export default function TaskForces() {
+export default function TaskForces({ loaderData }) {
+    const taskForcesData = loaderData;
+
     const taskForces = [];
-    tfInfoTemp.map((tf) => {
+    taskForcesData.map((tf) => {
         taskForces.push(<TaskForceSection key={tf.url} taskForceInfo={tf}/>);
     })
 
